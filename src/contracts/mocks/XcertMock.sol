260bf964997b7a27ec6824d67e56de0646020625
Tadej Vengust <vengust.tadej@gmail.com>
2018-06-11 17:18:48 +0200

    Add xcert mock.

---
+++ src/contracts/mocks/XcertMock.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.23;
+
+import "@0xcert/ethereum-xcert/contracts/tokens/Xcert.sol";
+
+/**
+ * @dev This is an example implementation of the Xcert smart contract.
+ */
+contract XcertMock is Xcert {
+
+  /**
+   * @dev Contract constructor.
+   * @param _name A descriptive name for a collection of NFTs.
+   * @param _symbol An abbreviated name for NFT.
+   * @param _conventionId A bytes4 of keccak256 of json schema representing 0xcert protocol
+   * convention.
+   */
+  constructor(
+    string _name,
+    string _symbol,
+    bytes4 _conventionId
+  )
+    public
+  {
+    nftName = _name;
+    nftSymbol = _symbol;
+    nftConventionId = _conventionId;
+  }
+}
