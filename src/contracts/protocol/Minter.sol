b19055b53d4043bf622336ca6ec8822f4e0c30cc
Tadej Vengust <vengust.tadej@gmail.com>
2018-06-12 08:56:17 +0200

    Add constructor, get methods and tests for it.

--- src/contracts/protocol/Minter.sol
+++ src/contracts/protocol/Minter.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.4.24;
 pragma experimental ABIEncoderV2;
 
-
 import "@0xcert/ethereum-utils/contracts/math/SafeMath.sol";
 import "@0xcert/ethereum-utils/contracts/utils/SupportsInterface.sol";
 import "@0xcert/ethereum-utils/contracts/ownership/Ownable.sol";
@@ -11,11 +10,64 @@ import "./TokenTransferProxy.sol";
 import "./XcertMintProxy.sol";
 
 contract Minter is SupportsInterface
- {
+{
+  /*
+   * @dev contract addresses
+   */
+  address XCERT_MINT_PROXY_CONTRACT;
+  address ERC20_TOKEN_CONTRACT;
+  address TOKEN_TRANSFER_PROXY_CONTRACT;
 
-  constructor()
+  /*
+   * @dev Sets XCT token address, Token proxy address and xcert Proxy address.
+   * @param _xcertToken Address pointing to ERC20 Token contract.
+   * @param _tokenTransferProxy Address pointing to TokenTransferProxy contract.
+   * @param _XcertProxy Address pointing to XcertProxy contract.
+   */
+  constructor(
+    address _erc20Token,
+    address _tokenTransferProxy,
+    address _xcertMintProxy
+  )
     public
   {
+    ERC20_TOKEN_CONTRACT = _erc20Token;
+    TOKEN_TRANSFER_PROXY_CONTRACT = _tokenTransferProxy;
+    XCERT_MINT_PROXY_CONTRACT = _xcertMintProxy;
+    //supportedInterfaces[0xe0b725c2] = true; // Minter
   }
 
+  /*
+   * @dev Get address of token used in minter.
+   */
+  function getTokenAddress()
+    external
+    view
+    returns (address)
+  {
+    return ERC20_TOKEN_CONTRACT;
+  }
+
+  /*
+   * @dev Get address of token transfer proxy used in minter.
+   */
+  function getTokenTransferProxyAddress()
+    external
+    view
+    returns (address)
+  {
+    return TOKEN_TRANSFER_PROXY_CONTRACT;
+  }
+
+
+  /*
+   * @dev Get address of xcert mint proxy used in minter.
+   */
+  function getXcertMintProxyAddress()
+    external
+    view
+    returns (address)
+  {
+    return XCERT_MINT_PROXY_CONTRACT;
+  }
 }
--- src/tests/Minter.test.ts
+++ src/tests/Minter.test.ts
@@ -1,10 +1,33 @@
 import { assert, artifact, web3 } from '@chainspin/test';
 
 describe('Minter', async () => {
-
+  
   beforeEach(async () => {
-    this.minter = await artifact.deploy({ src: 'Minter.json' });
+    this.tokenProxy = await artifact.deploy({ src: 'TokenTransferProxy.json' });
+    this.mintProxy = await artifact.deploy({ src: 'XcertMintProxy.json' });
+    this.token = await artifact.deploy({ src: 'TokenMock.json' });
+    this.minter = await artifact.deploy({
+      src: 'Minter.json', 
+      args: [this.token._address, this.tokenProxy._address, this.mintProxy._address] 
+    });
     this.accounts = await web3.eth.getAccounts();
+  });
+
+  describe('contract addresses', async () => {
+    it('check if token address is correct', async () => {
+      let address = await this.minter.methods.getTokenAddress().call({ from: this.accounts[0] });
+      assert.equal(address, this.token._address);
+    });
+
+    it('check if token transfer proxy address is correct', async () => {
+      let address = await this.minter.methods.getTokenTransferProxyAddress().call({ from: this.accounts[0] });
+      assert.equal(address, this.tokenProxy._address);
+    });
+
+    it('check if xcert mint proxy address is correct', async () => {
+      let address = await this.minter.methods.getXcertMintProxyAddress().call({ from: this.accounts[0] });
+      assert.equal(address, this.mintProxy._address);
+    });
   });
 
   it('Does nothing - test', async () => {
