e99c7e8a237de616de2fab338694b653beb0b8a6
Tadej Vengust <vengust.tadej@gmail.com>
2018-06-12 08:29:28 +0200

    Add imports.

--- src/contracts/protocol/Minter.sol
+++ src/contracts/protocol/Minter.sol
@@ -2,7 +2,16 @@ pragma solidity ^0.4.24;
 pragma experimental ABIEncoderV2;
 
 
-contract Minter {
+import "@0xcert/ethereum-utils/contracts/math/SafeMath.sol";
+import "@0xcert/ethereum-utils/contracts/utils/SupportsInterface.sol";
+import "@0xcert/ethereum-utils/contracts/ownership/Ownable.sol";
+import "@0xcert/ethereum-xcert/contracts/tokens/Xcert.sol";
+import "@0xcert/ethereum-erc20/contracts/tokens/ERC20.sol";
+import "./TokenTransferProxy.sol";
+import "./XcertMintProxy.sol";
+
+contract Minter is SupportsInterface
+ {
 
   constructor()
     public
