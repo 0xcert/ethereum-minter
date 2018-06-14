b67a18e185c0a0fcc56a0a2501dcff478306ee02
Tadej Vengust <vengust.tadej@gmail.com>
2018-06-14 09:47:04 +0200

    Add assertRevert for tests.

---
+++ src/tests/helpers/assertRevert.ts
@@ -0,0 +1,11 @@
+import { assert } from '@chainspin/test';
+
+export default async function(promise) {
+  try {
+    await promise;
+    assert.fail('Expected revert not received');
+  } catch (error) {
+    const revertFound = error.message.search('revert') >= 0;
+    assert(revertFound, `Expected "revert", got ${error} instead`);
+  }
+};
