f741b21a3f5e33796a0476ad2fea922e4bd6f2e8
Tadej Vengust <vengust.tadej@gmail.com>
2018-06-14 09:53:27 +0200

    Add todo and commented out hashing test code.

--- src/tests/protocol/Minter.test.ts
+++ src/tests/protocol/Minter.test.ts
@@ -96,16 +96,117 @@ describe('Minter', function () {
     });
   });
 
+  /*
+  TODO(Tadej): Find a way to locally hash array of structs.
   describe('hashing', function () {
+    let testArrayAccount;
+    let testArrayAmount;
+
+    let timestamp;
+    let expirationTimestamp ;
+
+    let claimUintArray;
+    let claimBytesArray;
+
+    let contractHash;
+
     beforeEach(async function () {
+      testArrayAccount = [accounts[3], accounts[5]];
+      testArrayAmount = [1, 10];
+
+      timestamp = 1521195657;
+      expirationTimestamp = 1821195657;
+
+      claimUintArray = [id1, timestamp, expirationTimestamp, 1, 1, 10];
+      claimBytesArray = ["0x1", "0x3", config[0], data[0]];
+
+      const xcertData = {
+        xcert: xcert._address,
+        id: id1,
+        proof: mockProof,
+        uri: uri,
+        config: config,
+        data: data,
+      }
+
+      const mintData = {
+        to: accounts[1],
+        fees: [
+          {
+            feeAddress: accounts[3],
+            feeAmount: 1,
+            tokenAddress: token._address,
+          },
+          {
+            feeAddress: accounts[5],
+            feeAmount: 10,
+            tokenAddress: token._address,
+          }
+        ],
+        seed: timestamp,
+        expirationTimestamp: expirationTimestamp,
+      }
+
+      contractHash = await minter.methods
+        .getMintDataClaim(toTuple(mintData), toTuple(xcertData))
+        .call({
+          from: accounts[0]
+        });
     });
 
     it('compares the same local and contract hash', async function () {
+
+      const test = {
+        amount: 50,
+        data: [
+          {
+            address: '0x627306090abaB3A6e1400e9345bC60c78a8BEf57',
+            value: 50
+          },
+          {
+            address: '0x627306090abaB3A6e1400e9345bC60c78a8BEf57',
+            value: 50
+          },
+          {
+            address: '0x627306090abaB3A6e1400e9345bC60c78a8BEf57',
+            value: 50
+          }
+        ]
+      }
+
+      console.log(toTuple(test));
+      console.log(toTuple(test).join(','));
+
+
+      console.log(contractHash);
+      const fees = {
+        fees: [
+          {
+            feeAddress: accounts[3],
+            feeAmount: 1,
+            tokenAddress: token._address,
+          },
+          {
+            feeAddress: accounts[5],
+            feeAmount: 10,
+            tokenAddress: token._address,
+          }
+        ]
+      }
+
+      let localHash = web3.utils.soliditySha3(minter._address, accounts[1], xcert._address, id1, mockProof, uri,
+        {t: 'bytes32[]', v:config}, {t: 'bytes32[]', v:data},
+        toTuple(fees).join(","), timestamp, expirationTimestamp);
+      assert.equal(contractHash, localHash);
     });
 
     it('compares different local and contract hash', async function () {
+       let localHash = web3.utils.soliditySha3(minter._address, accounts[1], xcert._address, id1, mockProof, uri,
+        {t: 'bytes32[]', v:config}, {t: 'bytes32[]', v:data},
+        {t: 'address[]', v:testArrayAccount}, {t: 'uint256[]', v:testArrayAmount}, timestamp, 34);
+      assert.notEqual(contractHash, localHash);
     });
-  });
+  });*/
 
 
   describe('signature', function () {
